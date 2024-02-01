e3e447337c121ee600cb651f92521f9159c2e1b9
rayyanazywh <93581397+rayyanazywh@users.noreply.github.com>
2024-02-01 20:23:19 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//function preload()
+function preLoad(){
+}
+
+//setup()
+function setUp(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+// draw()
+function draw(){
+}
+
+// take_snapshot
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
